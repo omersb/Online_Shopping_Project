7133bb0cf2989b32ac3bb99011421a1bfae21b10
omersb <omersaidbuldukk@gmail.com>
2022-12-24 16:32:48 +0300

    app and navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./componets/Home";
+import Navbar from "./componets/Navbar";
+import Products from "./componets/Products";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
